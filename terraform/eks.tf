ace52e2e4c989a405bfa55f0ab57be92679da64f
shashi <zero182004@gmail.com>
2026-06-03 21:21:27 +0530

    feat: deploy app to EKS and install ArgoCD

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -14,8 +14,8 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 1
-      min_size     = 1
+      desired_size = 2
+      min_size     = 2
       max_size     = 2
 
       instance_types = ["t3.small"]
